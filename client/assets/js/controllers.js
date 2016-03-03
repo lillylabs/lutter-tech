70640fc8447c6a07230c2e13cb45a2dafcfcb8b9
Benedicte Raae <raae@bgraphic.no>
2016-03-03 21:37:57 +0100

    Changed long to lng, added possibility of setting map tiles and center in data.json

--- client/assets/js/controllers.js
+++ client/assets/js/controllers.js
@@ -21,15 +21,37 @@ angular.module('application')
   }])
 
   .controller('MapController', [ '$scope', '$state', '$http', 'DataSource', 'leafletMapEvents', 'leafletMarkerEvents', function($scope, $state, $http, DataSource, leafletMapEvents, leafletMarkerEvents) {
-    $scope.sights = {};
+
+    angular.extend($scope, {
+      // Setting temporary center to Oslo
+      center: {
+        lat: 59.914831,
+        lng: 10.767347,
+        zoom: 16
+      },
+      tiles: {
+        name: 'Positron',
+        url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
+        type: 'xyz'
+      },
+      sights: {}
+    });
 
     DataSource.getAudioTour().then(function(tour) {
+      if(tour.tiles) {
+        $scope.tiles = tour.tiles;
+      }
+
+      if(tour.center) {
+        $scope.center = tour.center;
+      }
+
       angular.forEach(tour.sections, function(section, sectionKey) {
         angular.forEach(section.sights, function(sight, sightKey) {
           $scope.sights[sight.slug] = {
             group: 'tour',
             lat: sight.location.lat,
-            lng: sight.location.long,
+            lng: sight.location.lng,
             icon: {
               type: 'div',
               iconSize: [24, 24],
@@ -40,23 +62,6 @@ angular.module('application')
           }
         });
       });
-    });
-
-
-    angular.extend($scope, {
-      // Setting center to Oslo
-      // TODO: Set according to tour points
-      center: {
-        lat: 59.914831,
-        lng: 10.767347,
-        zoom: 15
-      },
-      tiles: {
-        name: 'Positron',
-        url: 'http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png',
-        type: 'xyz'
-      },
-      markers: $scope.sights,
     });
 
     $scope.$on('leafletDirectiveMarker.click', function(event, args){
